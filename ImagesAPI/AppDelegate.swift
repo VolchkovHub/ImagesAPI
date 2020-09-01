afdcf1f2adc9c8d4f52ca9235b4004cb93c04b69
Volchkov Fedor <>
2020-09-01 12:13:13 +0300

    Setup initial VC.

--- ImagesAPI/AppDelegate.swift
+++ ImagesAPI/AppDelegate.swift
@@ -12,11 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let homeViewController = UIViewController()
-        homeViewController.view.backgroundColor = UIColor.red
-        window!.rootViewController = homeViewController
-        window!.makeKeyAndVisible()
         return true
     }
 
